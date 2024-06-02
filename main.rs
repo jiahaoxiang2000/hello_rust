54d17a982a613efb100e2c66f10544c59b4cec1f
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-06-02 17:57:05 +0800

    rand function add

--- main.rs
+++ main.rs
@@ -1,9 +1,20 @@
-use std::io::stdin;
+use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
+
     let mut guess = String::new();
-    stdin().read_line(&mut guess).expect("Failed to read line");
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
     println!("You guessed: {guess}");
 }
