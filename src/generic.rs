32eca185342f04b950e2ad4784ec8fbdc56e8186
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-06-23 21:28:30 +0800

    trait_example

--- src/generic.rs
+++ src/generic.rs
@@ -47,3 +47,50 @@ fn multi_generic_type() {
     assert_eq!(p3.x, 5);
     assert_eq!(p3.y, 'c');
 }
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+#[test]
+fn trait_example() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+    
+    assert_eq!(
+        tweet.summarize(),
+        "horse_ebooks: of course, as you probably already know, people"
+    );
+}
