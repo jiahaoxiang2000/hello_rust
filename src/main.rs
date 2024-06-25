732b3b08ed4b9260def4bc85a4d164aae9b05b6b
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-06-25 22:45:26 +0800

    use the new construct

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap();
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,9 +20,17 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
 
-    Config { query, file_path }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+    
 }
+
