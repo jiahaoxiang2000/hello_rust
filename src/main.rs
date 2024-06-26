31c77c865949cd6e4c6891b88f26c928bbc94aab
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-06-26 09:41:53 +0800

    create  `run` func to logic, on `main` to deal with the error for invoke `run`.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,14 +8,18 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
-    // --snip--
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
